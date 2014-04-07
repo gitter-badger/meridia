54dfb9b7740af5e64a380ee48effd8e2fefd66de
Jhon Cortes <jhone.cortesg@gmail.com>
2014-04-07 13:17:35 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meridia::Application.config.secret_key_base = 'bab1b5959e375559634f858d87a11af5a77e79c154e602fcbc6c3b3d8d206040b277fc7f02b39cdbe7f0531a6cb3fe2cbc1fcbad4de2113b4df1cb0d12ca8718'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Meridia::Application.config.session_store :cookie_store, key: '_meridia_session'
