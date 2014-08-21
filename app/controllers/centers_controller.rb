66400a1618d2a891a2da09b55fbebe639539f605
Ezequiel Suárez <ezesubu@gmail.com>
2014-08-20 19:20:39 -0500

    views center

--- app/controllers/centers_controller.rb
+++ app/controllers/centers_controller.rb
@@ -14,16 +14,44 @@ class CentersController < ApplicationController
   # GET /centers/1
   # GET /centers/1.json
   def show
-    @result = {}
-    @result['tickets'] = Prospect.count
-    @result['cancelaciones'] = Prospect.where(:status=>'Cancel').count
-    @result['ventas'] = Prospect.where(:status=>'Venta').count
-    @result['visitas'] = Ticket.where(:type=>'Cita').count
-    @date_start = (Time.now - 30.days).strftime("%F")
-    @date_end = (Time.now + 1.days).strftime("%F")
-    @data_member = ProspectMember.where(:created_at.gte=> @date_start, :created_at.lte =>@date_end)
-    @data = Prospect.where(:created_at.gte=> @date_start, :created_at.lte =>@date_end)
+    if current_user.roles.where(name: "Ventas").exists?
+      @result = {}
+      @result['tickets'] = Prospect.count
+      @result['cancelaciones'] = Prospect.where(:status=>'Cancel').count
+      @result['ventas'] = Prospect.where(:status=>'Venta').count
+      @result['visitas'] = Ticket.where(:type=>'Cita').count
+      @date_start = (Time.now - 30.days).strftime("%F")
+      @date_end = (Time.now + 1.days).strftime("%F")
+      @data_member = ProspectMember.where(:created_at.gte=> @date_start, :created_at.lte =>@date_end)
+      @data = Prospect.where(:created_at.gte=> @date_start, :created_at.lte =>@date_end)
+
+      render "show"
+    else
+      members=Member.all
+      @members = {}
+      @mental =[]
+      @members['all'] = members.count
+      @members['female'] = members.where(gender: 'female').count
+      @members['male'] = members.where(gender: 'male').count
+      sum_years = 0
+      con_edad = 0
+     
+      members.each do |member|
+        if member['date_borth'] && @members['all'] > 0      
+         sum_years += age(member['date_borth'])
+         con_edad += 1
+        end       
+      end
+      
+      gon.mental = arr_cognitiva(members)
+      gon.dependencia = arr_dependencia(members)   
+
+      @members['edad_promedio']=sum_years > 0 ? (sum_years/con_edad) : 0
+
+      render  "show1"
+    end
   end
+
   def show1
     members=Member.all
     @members = {}
