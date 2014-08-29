aca4b2a090d46f1303b713e80dd6c5e317229f93
Jhon Cortes <jhone.cortesg@gmail.com>
2014-08-29 11:17:10 -0500

    zona horaria

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  config.time_zone = 'America/Mexico_City'
 	protect_from_forgery with: :exception
 	before_filter  :set_multitenancy
 	
