27080fcb9faf41292a4982095b4749816e05219a
Ezequiel Suárez <ezesubu@gmail.com>
2014-09-08 11:45:27 -0500

    commit changes in september 5

--- app/controllers/payment/reports_controller.rb
+++ app/controllers/payment/reports_controller.rb
@@ -5,9 +5,21 @@ class Payment::ReportsController < ApplicationController
   def index
     arr_hours= Array.new(11){0}
     members = 0;
-    @attends =  Attend.where(created_at: (30.days.ago..Time.now)).desc
-    @attends = @attends.order_by(:entry_time => :asc)
-    @attends.each do |attend|
+   #@attends =  Attend.where(created_at: (30.days.ago..Time.now)).desc
+     if params[:search].nil?
+      @date_start = (Time.now - 30.days).strftime("%F")
+      @date_end = (Time.now + 1.days).strftime("%F")
+    else
+      @date_start = params[:search][:start_date]
+      @date_end = params[:search][:date_end]
+    end
+    data_member = Attend.where(:created_at.gte=> @date_start, :created_at.lte =>@date_end).asc(:created_at)
+    members_enter=  data_member.group_by { |m| m.entry_time.beginning_of_hour }    
+    members_out=  data_member.group_by { |m| m.entry_time.beginning_of_hour }    
+    #binding.pry
+
+    unless  @data_member.blank?
+      @data_member.each do |attend|
         hour = attend.entry_time.hour
         case hour.to_i
         when 8          
@@ -45,7 +57,7 @@ class Payment::ReportsController < ApplicationController
           (10..11).map{|n| arr_hours[n]=members}        
         end        
     end
-    @attends.each do |attend|
+    @data_member.each do |attend|
         hour = attend.exit_time.hour
         case hour.to_i
         when 8          
@@ -82,6 +94,9 @@ class Payment::ReportsController < ApplicationController
           (10..11).map{|n| arr_hours[n]=members}
         end    
    end    
+
+      
+    end
     gon.members = arr_hours
   end  
 
